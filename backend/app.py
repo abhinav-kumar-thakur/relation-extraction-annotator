6e33d150d9776e51336d363cd55016e5273ceb58
Abhinav Kumar Thakur <abhinavkumarthakur786@gmail.com>
2022-05-27 16:43:59 -0700

    Abhinav | Add global pymongo db session object

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from blueprints.ner import ner_bp
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+app.config['MONGO_URI'] = 'mongodb://root:example@localhost:27017/learning_e2e?authSource=admin&readPreference=primary&ssl=false'
 app.register_blueprint(ner_bp, url_prefix='/ner')
 
---
+++ backend/db/db.py
@@ -0,0 +1,13 @@
+from flask import current_app, g
+from werkzeug.local import LocalProxy
+from flask_pymongo import PyMongo
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = PyMongo(current_app).db
+       
+    return db
+
+
+db = LocalProxy(get_db)
