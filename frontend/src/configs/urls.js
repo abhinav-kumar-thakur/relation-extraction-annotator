d074e67ea4bfb0591735b8b382debfd8a239b8d6
abhinav-kumar-thakur <abhinavkumarthakur786@gmail.com>
2022-07-27 15:41:15 -0700

    Abhinav [#8] Create react FileUploader component

---
+++ frontend/src/configs/urls.js
@@ -0,0 +1,7 @@
+const BackendUrl = 'api/ner';
+
+export const TypeFileUploadURL = BackendUrl + '/types/upload';
+export const DataFileUploadURL = BackendUrl + '/raw/upload';
+
+export const DownloadAllURL = BackendUrl + '/all/download';
+export const DownloadApprovedURL = BackendUrl + '/approved/download';
--- frontend/src/pages/admin/Admin.js
+++ frontend/src/pages/admin/Admin.js
@@ -1,61 +1,9 @@
-import React, {useState} from 'react';
-import {Col, Row, Container, Stack, Tab, Tabs, InputGroup} from "react-bootstrap";
+import React from 'react';
+import {Container, Row, Tab, Tabs} from "react-bootstrap";
+import {FileUploader} from "../../components/fileuploader/FileUploader";
+import {TypeFileUploadURL, DataFileUploadURL, DownloadAllURL, DownloadApprovedURL} from "../../configs/urls"
 
 export function Admin() {
-    const backend_url = 'api/ner';
-
-    // NER Types file states
-    const [selectedEntityTypesFile, setSelectedEntityTypesFile] = useState();
-    const [isEntityTypesFileSelected, setIsEntityTypesFileSelected] = useState(false);
-    const [uploadEntityButtonText, setUploadEntityButtonText] = useState('Upload');
-
-    // data file states
-    const [selectedDataFile, setSelectedDataFile] = useState();
-    const [isDataFileSelected, setIsDataFileSelected] = useState(false);
-    const [uploadDataFileButtonText, setUploadDataFileButtonText] = useState('Upload');
-
-    const entityTypesFileChangeHandler = (event) => {
-        setSelectedEntityTypesFile(event.target.files[0]);
-        setUploadEntityButtonText('Upload');
-        setIsEntityTypesFileSelected(true);
-    };
-
-    const handleNERFileSubmission = () => {
-        const formData = new FormData();
-        formData.append('File', selectedEntityTypesFile);
-        fetch(backend_url + '/types/upload', {method: 'POST', body: formData})
-            .then((response) => response.json())
-            .then((result) => {
-                console.log('Success:', result);
-                setUploadEntityButtonText('Uploaded');
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-                setUploadEntityButtonText('Failed');
-            });
-    };
-
-    const handleDataFileSubmission = () => {
-        const formData = new FormData();
-        formData.append('File', selectedDataFile);
-        fetch(backend_url + '/raw/upload', {method: 'POST', body: formData})
-            .then((response) => response.json())
-            .then((result) => {
-                console.log('Success:', result);
-                setUploadDataFileButtonText('Uploaded');
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-                setUploadDataFileButtonText('Failed');
-            });
-    };
-
-    const dataFileChangeHandler = (event) => {
-        setSelectedDataFile(event.target.files[0]);
-        setUploadDataFileButtonText('Upload');
-        setIsDataFileSelected(true);
-    };
-
     return (<Tabs
         defaultActiveKey="uploads"
         id="uncontrolled-tab-example"
@@ -65,22 +13,10 @@ export function Admin() {
             <div>
                 <Container>
                     <Row>
-                        <h4>Types file</h4>
-                        <InputGroup className="mb-3">
-
-                            <input type="file" onChange={entityTypesFileChangeHandler}/>
-                            <button onClick={handleNERFileSubmission}
-                                    disabled={!isEntityTypesFileSelected}>{uploadEntityButtonText}</button>
-                        </InputGroup>
+                        <FileUploader title="Types file" upload_url={TypeFileUploadURL} />
                     </Row>
                     <Row>
-                        <h4>Data file</h4>
-                        <InputGroup className="mb-3">
-
-                            <input type="file" onChange={dataFileChangeHandler}/>
-                            <button onClick={handleDataFileSubmission}
-                                    disabled={!isDataFileSelected}>{uploadDataFileButtonText}</button>
-                        </InputGroup>
+                        <FileUploader title="Data file" upload_url={DataFileUploadURL} />
                     </Row>
                 </Container>
             </div>
@@ -88,10 +24,10 @@ export function Admin() {
         <Tab eventKey="downloads" title="Downloads">
             <Container>
                 <Row>
-                    <a href={backend_url + '/all/download'}>All</a>
+                    <a href={DownloadAllURL}>All</a>
                 </Row>
                 <Row>
-                    <a href={backend_url + '/approved/download'} style={{'marginRight': '1%'}}>Approved</a>
+                    <a href={DownloadApprovedURL}>Approved</a>
                 </Row>
             </Container>
         </Tab>
