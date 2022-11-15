e2ead5f632e2b99fde98c872cfa0265ea31159bd
abhinav-kumar-thakur <abhinavkumarthakur786@gmail.com>
2022-11-15 11:34:27 -0800

    Make deployed backend API modules configurable

--- backend/app.py
+++ backend/app.py
@@ -3,19 +3,24 @@ from os import environ
 from flask import Flask
 from flask_cors import CORS
 
-from blueprints.admin import admin_bp
-from blueprints.data import data_bp
-from blueprints.multi_modal import multi_modal_bp
-
 app = Flask(__name__)
 
 # TODO: Add more specific CORS options
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.config['MONGO_URI'] = environ.get('MONGO_URI')
-app.register_blueprint(data_bp, url_prefix='/api/data')
-app.register_blueprint(admin_bp, url_prefix='/api/admin')
-app.register_blueprint(multi_modal_bp, url_prefix='/api/multi-modal')
+
+modules = environ.get('MODULES', 're,multi_modal').split(',')
+if 're' in modules:
+    from blueprints.admin import admin_bp
+    from blueprints.data import data_bp
+
+    app.register_blueprint(data_bp, url_prefix='/api/data')
+    app.register_blueprint(admin_bp, url_prefix='/api/admin')
+
+if 'multi_modal' in modules:
+    from blueprints.multi_modal import multi_modal_bp
+    app.register_blueprint(multi_modal_bp, url_prefix='/api/multi_modal')
 
 
 @app.route('/status')
